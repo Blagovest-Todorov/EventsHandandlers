70c7a7f519c257e67af76ff804c188e8628e606a
Blagovest-Todorov <blagovest.stod@abv.bg>
2023-02-15 13:15:13 +0200

    written file with async methods

---
+++ TestAp/Synchronous_Programing/AsyncronosProgram.cs
@@ -0,0 +1,27 @@
+﻿namespace TestAp.Synchronous_Programing
+{
+    public class AsyncronosProgram
+    {
+        static async Task Main(string[]args) 
+        {
+            LongProcess();
+            ShortProcess();
+        }
+
+
+        private static async void LongProcess()
+        {
+            Console.WriteLine("LongProcess Started");
+            await Task.Delay(4000); // hold execution for 4 second.
+            Console.WriteLine("LongProcess Completed");
+        }
+        private static async void ShortProcess()
+        {
+            Console.WriteLine("ShortProcess Started.");
+
+            // do something here
+
+            Console.WriteLine("ShortProcess Completed.");
+        }
+    }
+}
